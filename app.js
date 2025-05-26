9a65e5ccf5113efcd152be766341a885427ec17b
mirzahaifzh <mirzafio78@gmail.com>
2025-05-26 16:08:53 +0800

    vercel.json

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');          // <-- tambahkan ini
 const userRoutes = require('./routes/userRoutes');
 const menstrualCycleRoutes = require('./routes/menstrualCycleRoutes');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+app.use(cors());                      // <-- tambahkan ini
 app.use(bodyParser.json());
 
 // Routes
 app.use('/users', userRoutes);
 app.use('/menstrual-cycles', menstrualCycleRoutes);
+
 // Root endpoint
 app.get('/', (req, res) => {
   res.send('API is running...');
