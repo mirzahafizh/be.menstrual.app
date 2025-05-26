459667a31d5a643949b2bbbcaff237a7a8c04031
mirzahaifzh <mirzafio78@gmail.com>
2025-05-26 16:33:49 +0800

    vercel.json

--- controllers/menstrualController.js
+++ controllers/menstrualController.js
@@ -48,6 +48,7 @@ async create(req, res) {
     const fertile_window_end = new Date(ovulation_date);
     fertile_window_end.setDate(fertile_window_end.getDate() + 1);
 
+    
     const cycle = await MenstrualCycle.create({
       user_id,
       start_date,
